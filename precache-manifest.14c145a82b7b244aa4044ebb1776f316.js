2e87244918f467f3d6e220e38abc5c4c64b078da
Javier <akabane02013@gmail.com>
2020-08-23 18:10:09 -0400

    Updates

---
+++ precache-manifest.14c145a82b7b244aa4044ebb1776f316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18fba89495a243c63181dac22417d87",
+    "url": "/scriptx/index.html"
+  },
+  {
+    "revision": "2032df2ce0d7323f85e7",
+    "url": "/scriptx/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "022376173215815fd09a",
+    "url": "/scriptx/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2032df2ce0d7323f85e7",
+    "url": "/scriptx/static/js/2.b724bfe9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/scriptx/static/js/2.b724bfe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022376173215815fd09a",
+    "url": "/scriptx/static/js/main.e2973d07.chunk.js"
+  },
+  {
+    "revision": "8374eeb080c71408e9f9",
+    "url": "/scriptx/static/js/runtime-main.55bec465.js"
+  }
+]);
